b58132abbc18ee1ade73fef77a8cbe663faabf89
IonutBocos <101240913+IonutBocos@users.noreply.github.com>
2022-09-15 09:38:19 +0300

    Exercitii clasa 14.09

---
+++ app-contact-manager/modules/data.js
@@ -0,0 +1,16 @@
+export default [
+  {
+    id: 1,
+    name: 'Carol',
+    surname: 'Carolson',
+    phone: '074465555',
+    email: 'carol@carol.ro',
+  },
+  {
+    id: 2,
+    name: 'Larry',
+    surname: 'Laryson',
+    phone: '07558558',
+    email: 'wtv@larry.ro',
+  },
+];
