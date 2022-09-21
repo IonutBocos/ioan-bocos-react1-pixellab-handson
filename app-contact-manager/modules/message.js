c6187b23ea8c4c417e1511089ae79b90d819c7b8
IonutBocos <101240913+IonutBocos@users.noreply.github.com>
2022-09-21 19:06:23 +0300

    add contact btn

--- app-contact-manager/modules/message.js
+++ app-contact-manager/modules/message.js
@@ -2,6 +2,7 @@ export default (message = '', type = 'primary') => {
   const messageContainer = document.createElement('div');
   messageContainer.classList.add('alert', `alert-${type}`);
 
+  // FYI exista un textContent
   messageContainer.textContent = message;
 
   return messageContainer;
